5cad75e75354d99b80d4efb4b4a5542ed1d55a8e
Reza <khalidreza6@gmail.com>
2021-11-16 19:32:57 +0530

    make it better

--- API/Extension/ApplicationServiceExtension.cs
+++ API/Extension/ApplicationServiceExtension.cs
@@ -55,11 +55,11 @@ namespace API.Extension
                                 var pgPort = pgHostPort.Split(":")[1];
 
                                 connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
-    }
+                               }
 
 // Whether the connection string came from the local development configuration file
 // or from the environment variable from Heroku, use it to set up your DbContext.
-options.UseNpgsql(connStr);
+              options.UseNpgsql(connStr);
                 //options.UseNpgsql(config.GetConnectionString("DefalutConnection"));
             });
             services.AddScoped<IUserRepository,UserRepository>();
